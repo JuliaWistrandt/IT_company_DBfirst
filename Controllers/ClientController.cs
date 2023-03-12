a4b280cb86d048a5aee2705d8455896939e2e550
Julia Ang <lecrak1988@gmail.com>
2023-03-12 22:18:36 +0100

    Add project files.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,100 @@
+﻿using ItAgency6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItAgency6.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ItAgencyContext _context;
+
+        public ClientController(ItAgencyContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Client> clients = _context.Clients.ToList();
+            return View(clients);
+        }
+
+        [HttpGet]
+        public IActionResult Details(string id) //не работает, не присылает id
+        {
+            int temp = Int32.Parse(id);
+            Client client = _context.Clients.Where(client => client.Id == temp).FirstOrDefault();
+            return View(client.Id);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            int temp = Int32.Parse(id);
+            Client client = _context.Clients.Where(client => client.Id == temp).FirstOrDefault();
+            return View(client);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Client client)
+        {
+            _context.Attach(client);
+            _context.Entry(client).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            int temp = Int32.Parse(id);
+            Client client = _context.Clients.Where(client => client.Id == temp).FirstOrDefault();
+            return View(client);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Client client)
+        {
+            _context.Attach(client);
+            _context.Entry(client).State = EntityState.Deleted;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Client client = new Client();
+            return View(client);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Client client)
+        {
+            var lastCustomer = _context.Clients
+                 .OrderByDescending(client => client.Id)
+                 .FirstOrDefault();
+
+            int nextCustomerId;
+            if (lastCustomer == null)
+            {
+                throw new InvalidOperationException("No last ID found in the database.");
+            }
+            else
+            {
+                nextCustomerId = lastCustomer.Id + 1;
+            }
+
+            return View("Details", nextCustomerId);
+
+
+        }
+
+    }
+}
---
+++ Migrations/20230312202247_InitialSetup.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItAgency6.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Client__E67E1A0410155368", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Departme__1C54560715244945", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItAgency",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__ItAgency__2D971C4C1B787490", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(10,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Position__60BB9A592ADF4A90", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "date", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "date", nullable: true),
+                    ClientID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Project__761ABED0BEEC6794", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ProjectClient",
+                        column: x => x.ClientID,
+                        principalTable: "Client",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Employee__7AD04FF16CD6F6A1", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK__Employee__Depart__47DBAE45",
+                        column: x => x.DepartmentID,
+                        principalTable: "Department",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK__Employee__Positi__48CFD27E",
+                        column: x => x.Position,
+                        principalTable: "Position",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tool",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DailyCosts = table.Column<decimal>(type: "decimal(10,2)", nullable: true),
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Tool__CC0CEBB116B63563", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK__Tool__ProjectID__4E88ABD4",
+                        column: x => x.ProjectID,
+                        principalTable: "Project",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Name",
+                table: "Client",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentID",
+                table: "Employee",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_Position",
+                table: "Employee",
+                column: "Position");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ClientID",
+                table: "Project",
+                column: "ClientID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tool_ProjectID",
+                table: "Tool",
+                column: "ProjectID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "ItAgency");
+
+            migrationBuilder.DropTable(
+                name: "Tool");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Position");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
